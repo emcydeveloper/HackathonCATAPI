a9ec07a40e7d30c45f2946a5c319e525943fe4be
emcydeveloper <91327979+emcydeveloper@users.noreply.github.com>
2021-11-22 01:34:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ const paths = {
 // Filter: "pixel" | "paint" | "mono" | "blur" | "sepia" | "negative"
 
 let tagAndID = [];
+let searchResult = 0;
+/*
+0 - True
+1 - Minimum Keywords of 3 letters is required
+2 - No data found
+*/
 
 let getMainContainer = document.getElementById("mainContainer");
 let getSearchDiv = document.getElementById("searchDiv");
@@ -71,55 +77,102 @@ async function loadApiData() {
     // Loading all the images from API as default
     loadAllImg(tagAndID);
 
-    // Creating click event on sear`h button for the text entered on te`t box
+    // Creating click event on search button for the text entered on text box
     createBtn.addEventListener("click", () => {
+      headingText.innerHTML = "";
+      getSearchDiv.appendChild(headingText);
       tagInput = document.getElementById("searchText").value;
       tagInput = tagInput.toLowerCase();
       if (tagInput.length > 2) {
         let collectID = [];
         tagAndID.forEach((data) => {
           if (data.tag == tagInput) {
+            searchResult = 0;
             let objTagAndID = { id: data.id, tag: data.tag };
             collectID.push(objTagAndID);
           } else {
-            headingText.innerHTML = "No data found";
-            getSearchDiv.appendChild(headingText);
+            searchResult = 2;
           }
         });
-        headingText.innerHTML = "Enter characters to search..";
-        getSearchDiv.appendChild(headingText);
-        loadAllImg(collectID);
+        if (collectID.length > 1) {
+          searchResult = 0;
+          loadAllImg(collectID);
+        } else {
+          searchResult = 2;
+          //loadAllImg(tagAndID);
+        }
       } else {
+        searchResult = 1;
         loadAllImg(tagAndID);
+      }
+      /*
+0 - True
+1 - Minimum Keywords of 3 letters is required
+2 - No data found
+*/
+
+      switch (searchResult) {
+        case 0:
+          headingText.innerHTML = `Viewing cats with keyword ${tagInput}`;
+          getSearchDiv.appendChild(headingText);
+          break;
+        case 1:
+          headingText.innerHTML = `Minimum Keywords of 3 letters is required.`;
+          getSearchDiv.appendChild(headingText);
+          break;
+        case 2:
+          headingText.innerHTML = `No data found with keyword ${tagInput}`;
+          getSearchDiv.appendChild(headingText);
+          break;
       }
     });
 
     getSearchText.addEventListener("keyup", () => {
+      headingText.innerHTML = "";
+      getSearchDiv.appendChild(headingText);
       tagInput = document.getElementById("searchText").value;
       tagInput = tagInput.toLowerCase();
       if (tagInput.length > 2) {
         let collectID = [];
         tagAndID.forEach((data) => {
           if (data.tag == tagInput) {
+            searchResult = 0;
             let objTagAndID = { id: data.id, tag: data.tag };
             collectID.push(objTagAndID);
           } else {
-            headingText.innerHTML = "No data found";
-            getSearchDiv.appendChild(headingText);
+            searchResult = 2;
           }
         });
-        loadAllImg(collectID);
+        if (collectID.length > 1) {
+          searchResult = 0;
+          loadAllImg(collectID);
+        } else {
+          searchResult = 2; //loadAllImg(tagAndID);
+        }
       } else {
-        headingText.innerHTML = "Enter characters to search..";
-        getSearchDiv.appendChild(headingText);
+        searchResult = 1;
         loadAllImg(tagAndID);
+      }
+
+      switch (searchResult) {
+        case 0:
+          headingText.innerHTML = `Viewing cats with keyword ${tagInput}`;
+          getSearchDiv.appendChild(headingText);
+          break;
+        case 1:
+          headingText.innerHTML = `Minimum Keywords of 3 letters is required.`;
+          getSearchDiv.appendChild(headingText);
+          break;
+        case 2:
+          headingText.innerHTML = `No data found with keyword ${tagInput}`;
+          getSearchDiv.appendChild(headingText);
+          break;
       }
     });
   } catch (er) {
     console.log(er);
     headingText.innerHTML = `ERROR WHILE COOKING DATA FOR YOU.<BR>${er}`;
-        getSearchDiv.appendChild(headingText);
-    
+    getSearchDiv.appendChild(headingText);
   }
 }
 
@@ -132,7 +185,6 @@ function loadAllImg(getImgIDData) {
   getImgIDData.forEach((element) => {
     let getImgID = element.id;
 
-
     let createImgTag = document.createElement("img");
     createImgTag.setAttribute("id", getImgID);
     createImgTag.setAttribute("class", "domImg");
@@ -144,5 +196,4 @@ function loadAllImg(getImgIDData) {
     createImgTag.src = `https://cataas.com/cat/${getImgID}`;
     getOutputDiv.appendChild(createImgTag);
   });
-
 }
